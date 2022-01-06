3f76c9b7f042d70cf261d58811f991251b77c4fa
Adrian Tan <adrian.tan09@gmail.com>
2022-01-06 18:32:44 +1100

    completed day4 task1

--- day4/main.py
+++ day4/main.py
@@ -1,8 +1,12 @@
-from part1 import function1
+import re
+from part1 import scanner
 from part2 import function2
 
-f = open('./example.txt', 'r').read().split('\n')
-data = f
+with open('./input.txt', 'r') as fd:
+    content = fd.read()
+    lines  = content.split('\n\n')
+    passports = list(line.replace('\n', ' ') for line in lines)
+    passports = list(re.split(':| ', p) for p in passports)
 
-print('part 1:', function1(data))
-print('part 2:', function1(data))
+print('part 1:', scanner(passports))
+# print('part 2:', function1(data))
--- day4/part1.py
+++ day4/part1.py
@@ -1,3 +1,17 @@
 # Time complexity: 
-def function1(data):
-    return data
+def scanner(passports):
+    valid_passports = 0
+    for passport in passports:
+        if is_valid_passport(passport):
+            valid_passports += 1
+
+    return valid_passports
+
+def is_valid_passport(passport):
+    fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    field_count = 0
+    for field in fields:
+        if field in passport:
+            field_count += 1
+
+    return field_count == 7
