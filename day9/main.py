020c988ed4514774aad42c7a848a1aa0df41c021
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-02-20 16:44:11 +1100

    day9 part1 completed

--- day9/main.py
+++ day9/main.py
@@ -1,9 +1,10 @@
-from part1 import function1
+from part1 import find_first_fail_two_sum
 from part2 import function2
 
-with open('./example.txt', 'r') as fd:
+with open('./input.txt', 'r') as fd:
     content = fd.read()
     lines = content.split('\n')
+    lines = [int(n) for n in lines]
 
-print('part 1:', function1(lines))
-print('part 2:', function2(lines))
+print('part 1:', find_first_fail_two_sum(lines))
+# print('part 2:', function2(lines))
--- day9/part1.py
+++ day9/part1.py
@@ -1,3 +1,15 @@
-# Time complexity: 
-def function1(data):
-    return data
+def find_first_fail_two_sum(numbers):
+    preamble = 25
+    for i in range(preamble, len(numbers)):
+        if not two_sum(numbers[i-preamble:i], numbers[i]):
+            return numbers[i] 
+        
+    return -1
+
+def two_sum(numbers, target):
+    for i in numbers:
+        for j in numbers:
+            if i != j and i + j == target:
+                return True
+        
+    return False
