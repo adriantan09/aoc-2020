5fc6ff158866d62e2d7595f0428042e272450229
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-02-21 16:48:14 +1100

    day10 part1

--- day10/part1.py
+++ day10/part1.py
@@ -1,3 +1,10 @@
-# Time complexity: 
-def function1(data):
-    return data
+def find_jolt_diff(numbers):
+    numbers.sort()
+    jolt_diff = {1: 0, 2: 0, 3: 1}
+    jolt_diff[numbers[0]] += 1 # starting jolt
+
+    for i in range(1, len(numbers)):
+        diff = numbers[i] - numbers[i - 1]
+        jolt_diff[diff] += 1
+
+    return jolt_diff[1] * jolt_diff[3]
