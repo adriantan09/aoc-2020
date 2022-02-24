97928e1fcaa5e7b5c586f16e62a64566e8ff502a
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-02-24 21:32:00 +1100

    completed day11 part 1

--- day11/main.py
+++ day11/main.py
@@ -1,9 +1,10 @@
-from part1 import function1
+from part1 import part1
 from part2 import function2
 
-with open('./example.txt', 'r') as fd:
+with open('./input.txt', 'r') as fd:
     content = fd.read()
     lines = content.split('\n')
+    seats = [list(l) for l in lines]
 
-print('part 1:', function1(lines))
-print('part 2:', function2(lines))
+print('part 1:', part1(seats))
+# print('part 2:', function2(lines))
