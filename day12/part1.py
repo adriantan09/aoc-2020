60356a5c45b647d41666155a927c4e14f86d5082
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-03-03 23:34:02 +1100

    day12 part 1

--- day12/part1.py
+++ day12/part1.py
@@ -1,3 +1,14 @@
-# Time complexity: 
-def function1(data):
-    return data
+# Time complexity: O(n) where n is the number of directions
+def find_manhattan_dist(directions):
+    compass = {0: 0, 90: 0, 180: 0, 270: 0}
+    curr_degrees = 90
+    for d in directions:
+        if d[0] == 'N': compass[0] += d[1]
+        elif d[0] == 'E': compass[90] += d[1]
+        elif d[0] == 'S': compass[180] += d[1]
+        elif d[0] == 'W': compass[270] += d[1]
+        elif d[0] == 'F': compass[curr_degrees % 360] += d[1]
+        elif d[0] == 'L': curr_degrees -= d[1]
+        elif d[0] == 'R': curr_degrees += d[1]
+
+    return abs(compass[90] - compass[270]) + abs(compass[0] - compass[180])
