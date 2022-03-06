02f4001ace67e0bcbe083a2b2306f34cbf5efaa9
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-03-06 12:52:09 +1100

    day13 part1 complete

--- day13/main.py
+++ day13/main.py
@@ -1,9 +1,12 @@
 from part1 import function1
 from part2 import function2
 
-with open('./example.txt', 'r') as fd:
+with open('./input.txt', 'r') as fd:
     content = fd.read()
     lines = content.split('\n')
+    departure_time = int(lines[0])
+    bus_ids = list(filter(lambda id: id != 'x', lines[1].split(',')))
+    bus_ids = [int(id) for id in bus_ids]
 
-print('part 1:', function1(lines))
-print('part 2:', function2(lines))
+print('part 1:', function1(departure_time, bus_ids))
+# print('part 2:', function2(lines))
--- day13/part1.py
+++ day13/part1.py
@@ -1,3 +1,5 @@
-# Time complexity: 
-def function1(data):
-    return data
+def function1(departure_time, bus_ids):
+    wait_times = [id - (departure_time % id) for id in bus_ids]
+    shortest_wait = min(wait_times)
+    earliest_bus = bus_ids[wait_times.index(shortest_wait)]
+    return shortest_wait * earliest_bus
