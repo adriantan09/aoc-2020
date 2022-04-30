c3049bf2166e15a1c3d74edf683cf1f40f45bed7
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-04-30 12:20:11 +1000

    completed part 1 day 14

--- day14/part1.py
+++ day14/part1.py
@@ -1,3 +1,21 @@
-# Time complexity: 
-def function1(data):
-    return data
+import re
+
+def function1(instructions):
+    memory = {}
+    mask = 0
+    address = 0
+    value = 0
+    for instruction in instructions:
+        if 'mask' in instruction:
+            mask = instruction[7:]
+        if 'mem' in instruction:
+            address = int(re.search('[(0-9)]+', instruction).group(0))
+            value = int(re.search('(?<=] = )([0-9]+)', instruction).group(0))
+            value = str(bin(value))[2:].zfill(36)
+            result = value
+            for i in range(36):
+                if mask[i] != 'X':
+                    result = result[:i] + mask[i] + result[i+1:]
+            memory[address] = int(result, 2)
+    return sum(memory.values())
+    
