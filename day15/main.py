4ea5cba325fa296a729d8ac5c20cf49757c7870c
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-04-30 19:46:51 +1000

    completed day15 part 1

--- day15/main.py
+++ day15/main.py
@@ -4,7 +4,8 @@ from part1 import function1
 from part2 import function2
 
 assert len(sys.argv) == 2
-lines = open(sys.argv[1]).read().split('\n')
+numbers = open(sys.argv[1]).read().split(',')
+numbers = [int(n) for n in numbers]
 
-print('part 1:', function1(lines))
-print('part 2:', function2(lines))
+print('part 1:', function1(numbers))
+# print('part 2:', function2(numbers))
--- day15/part1.py
+++ day15/part1.py
@@ -1,3 +1,11 @@
-# Time complexity: 
-def function1(data):
-    return data
+def function1(spoken):
+    for i in range(len(spoken), 2020):
+        last_spoken = spoken[i - 1]
+        if last_spoken in spoken[:-1]:
+            last_found = [i for i, e in enumerate(spoken) if e == last_spoken][-1]
+            before_last_found = [i for i, e in enumerate(spoken[:last_found]) if e == last_spoken][-1]
+            spoken.append(last_found + 1 - (before_last_found + 1))
+        else:
+            spoken.append(0)
+
+    return spoken[-1]
