cf1e978e787459e92e97bfce2dea061a35c40ad4
adriantan09 <adrian.tan09@gmail.com>
2022-08-16 18:14:41 +1000

    completed part1

--- day16/main.py
+++ day16/main.py
@@ -1,10 +1,19 @@
 import sys
+import re
 
 from part1 import function1
 from part2 import function2
 
 assert len(sys.argv) == 2
-lines = open(sys.argv[1]).read().split('\n')
+lines = open(sys.argv[1]).read().split('\n\n')
 
-print('part 1:', function1(lines))
-print('part 2:', function2(lines))
+rules = [int(n) for n in re.findall('[0-9]+', lines[0])]
+your_ticket = [int(n) for n in re.findall('[0-9]+', lines[1])]
+nearby_tickets = [int(n) for n in re.findall('[0-9]+', lines[2])]
+
+# print(rules)
+# print(your_ticket)
+# print(nearby_tickets)
+
+print('part 1:', function1(rules, nearby_tickets))
+# print('part 2:', function2(lines))
--- day16/part1.py
+++ day16/part1.py
@@ -1,3 +1,14 @@
 # Time complexity: 
-def function1(data):
-    return data
+def function1(rules, nearby_tickets):
+    error_rate = 0
+    for ticket in nearby_tickets:
+        if not is_valid_ticket(rules, ticket):
+            error_rate += ticket
+    return error_rate
+
+def is_valid_ticket(rules, ticket):
+    for i in range(0, len(rules), 2):
+        if ticket in range(rules[i], rules[i + 1] + 1):
+            return True
+    return False
+
